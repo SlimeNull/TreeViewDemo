3cec5c36fbe8f9cff2be82d890726732f634a518
SlimeNull <slimenull@outlook.com>
2024-10-24 14:08:10 +0800

    Add project files.

---
+++ TreeViewDemo/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [ObservableObject]
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+
+            var rootNode = new GroupGroupTreeNode()
+            {
+                Name = "Root"
+            };
+
+            for (int i = 0; i < 30; i++)
+            {
+                var group = new EntityGroupTreeNode()
+                {
+                    Name = $"Group {i + 1}"
+                };
+
+                for (int j = 0; j < 300000; j++)
+                {
+                    group.Children.Add(new EntityTreeNode()
+                    {
+                        Name = System.IO.Path.GetRandomFileName()
+                    });
+                }
+
+                rootNode.Children.Add(group);
+            }
+
+            RootTreeNodes.Add(rootNode);
+
+            InitializeComponent();
+        }
+
+        public ObservableCollection<GroupGroupTreeNode> RootTreeNodes { get; } = new();
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(AddChildCommand))]
+        private ITreeNode? _selectedNode;
+
+
+        public bool CanAddChild()
+        {
+            return SelectedNode is GroupGroupTreeNode or EntityGroupTreeNode;
+        }
+
+        [RelayCommand(CanExecute = nameof(CanAddChild))]
+        public void AddChild()
+        {
+            if (SelectedNode is GroupGroupTreeNode groupGroupTreeNode)
+            {
+                groupGroupTreeNode.Children.Add(new EntityGroupTreeNode()
+                {
+                    Name = System.IO.Path.GetRandomFileName()
+                });
+            }
+            else if (SelectedNode is EntityGroupTreeNode entityGroupTreeNode)
+            {
+                entityGroupTreeNode.Children.Add(new EntityTreeNode()
+                {
+                    Name = System.IO.Path.GetRandomFileName(),
+                });
+            }
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (sender is not TreeView treeView)
+            {
+                return;
+            }
+
+            SelectedNode = treeView.SelectedItem as ITreeNode;
+        }
+    }
+}
---
+++ TreeViewDemo/TreeNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EleCho.WpfSuite.Markup
+{
+    record struct EmptyStruct;
+}
+
+namespace TreeViewDemo
+{
+    public interface ITreeNode
+    {
+        IEnumerable<ITreeNode> Children { get; }
+    }
+
+    public class GroupGroupTreeNode : ITreeNode
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public ObservableCollection<EntityGroupTreeNode> Children { get; } = new();
+
+        IEnumerable<ITreeNode> ITreeNode.Children => Children;
+    }
+
+    public class EntityGroupTreeNode : ITreeNode
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public ObservableCollection<EntityTreeNode> Children { get; } = new();
+
+        IEnumerable<ITreeNode> ITreeNode.Children => Children;
+    }
+
+    public class EntityTreeNode : ITreeNode
+    {
+        public string Name { get; set; } = string.Empty;
+        public int BlockNumber { get; set; }
+
+        public IEnumerable<ITreeNode> Children
+        {
+            get
+            {
+                yield break;
+            }
+        }
+    }
+}
